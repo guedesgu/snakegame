8cf8fcae1b869ee4d6875df5f08b0bfeb5ed1dc1
guedesgu <guguedesm@gmail.com>
2025-04-17 21:10:09 -0300

    Atualização do conteúdo
    Adicionado plano de fundo com base em imagem, substituindo o plano de fundo baseado em cor

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#importa biblioteca
 import pygame as pg
 from random import randrange
 
@@ -19,18 +20,25 @@ while True:
         if event.type == pg.QUIT:
             exit()
         if event.type == pg.KEYDOWN:
+            # movimento para cima
             if event.key == pg.K_w:
                 snake_dir = (0, -TILE_SIZE)
+            # movimento para baixo
             if event.key == pg.K_s:
                 snake_dir = (0, TILE_SIZE)
+            # movimento para esquerda    
             if event.key == pg.K_a:
                 snake_dir = (-TILE_SIZE, 0)
+            # movimento para direita    
             if event.key == pg.K_d:
                 snake_dir = (TILE_SIZE, 0)
-    screen.fill('black')
-    # draw snake
-    [pg.draw.rect(screen, 'green', segment) for segment in segments]
-    # move snake
+    # define uma imagem como valor da variavel           
+    bgTeste = pg.image.load('image.png')
+    # utiliza da imagem que foi definida no valor para aplicar no plano de fundo
+    screen.blit(bgTeste, (0, 0))
+    # desenha a cobra
+    [pg.draw.rect(screen, 'blue', segment) for segment in segments]
+    # configurações da movimentação da cobra
     time_now = pg.time.get_ticks()
     if time_now - time > time_step:
         time = time_now
